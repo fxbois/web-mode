5ed3013f42821de05cd1d1fa0894727ce1f83abe
François-Xavier Bois <fxbois@gmail.com>
2014-02-28 16:29:33 +0100

    remove parens

--- web-mode.el
+++ web-mode.el
@@ -3,7 +3,7 @@
 
 ;; Copyright 2011-2014 François-Xavier Bois
 
-;; Version: 8.0.22
+;; Version: 8.0.23
 ;; Author: François-Xavier Bois <fxbois AT Google Mail Service>
 ;; Maintainer: François-Xavier Bois
 ;; Created: July 2011
@@ -60,7 +60,7 @@
 ;;todo : commentaire d'une ligne ruby ou d'une ligne asp
 ;;todo : créer tag-token pour différentier de part-token : tag-token=attr,comment ???
 
-(defconst web-mode-version "8.0.22"
+(defconst web-mode-version "8.0.23"
   "Web Mode version.")
 
 (defgroup web-mode nil
@@ -266,42 +266,42 @@ with value 2, HTML lines beginning text are also indented (do not forget side ef
   :group 'web-mode)
 
 (defface web-mode-error-face
-  '((t (:background "red")))
+  '((t :background "red"))
   "Face for warning."
   :group 'web-mode-faces)
 
 (defface web-mode-warning-face
-  '((t (:inherit font-lock-warning-face)))
+  '((t :inherit font-lock-warning-face))
   "Face for warning."
   :group 'web-mode-faces)
 
 (defface web-mode-preprocessor-face
-  '((t (:inherit font-lock-preprocessor-face)))
+  '((t :inherit font-lock-preprocessor-face))
   "Face for preprocessor."
   :group 'web-mode-faces)
 
 (defface web-mode-block-delimiter-face
-  '((t (:inherit font-lock-preprocessor-face)))
+  '((t :inherit font-lock-preprocessor-face))
   "Face for block delimiters."
   :group 'web-mode-faces)
 
 (defface web-mode-block-control-face
-  '((t (:inherit font-lock-preprocessor-face)))
+  '((t :inherit font-lock-preprocessor-face))
   "Face for preprocessor."
   :group 'web-mode-faces)
 
 (defface web-mode-builtin-face
-  '((t (:inherit font-lock-builtin-face)))
+  '((t :inherit font-lock-builtin-face))
   "Face for builtins."
   :group 'web-mode-faces)
 
 (defface web-mode-symbol-face
-  '((t (:foreground "gold")))
+  '((t :foreground "gold"))
   "Face for symbols."
   :group 'web-mode-faces)
 
 (defface web-mode-doctype-face
-  '((t (:foreground "Grey")))
+  '((t :foreground "Grey"))
   "Face for HTML doctype."
   :group 'web-mode-faces)
 
@@ -317,7 +317,7 @@ with value 2, HTML lines beginning text are also indented (do not forget side ef
   :group 'web-mode-faces)
 
 (defface web-mode-html-tag-custom-face
-  '((t (:inherit web-mode-html-tag-face)))
+  '((t :inherit web-mode-html-tag-face))
   "Face for HTML custom tags (e.g. <polymer-element>)."
   :group 'web-mode-faces)
 
@@ -345,132 +345,132 @@ with value 2, HTML lines beginning text are also indented (do not forget side ef
   :group 'web-mode-faces)
 
 (defface web-mode-html-attr-custom-face
-  '((t (:inherit web-mode-html-attr-name-face)))
+  '((t :inherit web-mode-html-attr-name-face))
   "Face for custom attribute names (e.g. data-*)."
   :group 'web-mode-faces)
 
 (defface web-mode-html-attr-equal-face
-  '((t (:inherit web-mode-html-attr-name-face)))
+  '((t :inherit web-mode-html-attr-name-face))
   "Face for the = character between name and value."
   :group 'web-mode-faces)
 
 (defface web-mode-html-attr-value-face
-  '((t (:inherit font-lock-string-face)))
+  '((t :inherit font-lock-string-face))
   "Face for HTML attribute values."
   :group 'web-mode-faces)
 
 (defface web-mode-block-attr-name-face
-  '((t (:foreground "#8fbc8f"))) ;; inherit web-mode-html-attr-name-face))
+  '((t :foreground "#8fbc8f")) ;; inherit web-mode-html-attr-name-face))
   "Face for block attribute names."
   :group 'web-mode-faces)
 
 (defface web-mode-block-attr-value-face
-  '((t (:inherit web-mode-html-attr-value-face)))
+  '((t :inherit web-mode-html-attr-value-face))
   "Face for block attribute values."
   :group 'web-mode-faces)
 
 (defface web-mode-css-selector-face
-  '((t (:inherit font-lock-keyword-face)))
+  '((t :inherit font-lock-keyword-face))
   "Face for CSS rules."
   :group 'web-mode-faces)
 
 (defface web-mode-css-pseudo-class-face
-  '((t (:inherit font-lock-builtin-face)))
+  '((t :inherit font-lock-builtin-face))
   "Face for CSS pseudo-classes."
   :group 'web-mode-faces)
 
 (defface web-mode-css-at-rule-face
-  '((t (:inherit font-lock-constant-face)))
+  '((t :inherit font-lock-constant-face))
   "Face for CSS at-rules."
   :group 'web-mode-faces)
 
 (defface web-mode-css-property-name-face
-  '((t (:inherit font-lock-variable-name-face)))
+  '((t :inherit font-lock-variable-name-face))
   "Face for CSS props."
   :group 'web-mode-faces)
 
 (defface web-mode-css-color-face
-  '((t (:inherit font-lock-builtin-face)))
+  '((t :inherit font-lock-builtin-face))
   "Face for CSS colors (#xxx)."
   :group 'web-mode-faces)
 
 (defface web-mode-css-priority-face
-  '((t (:inherit font-lock-builtin-face)))
+  '((t :inherit font-lock-builtin-face))
   "Face for CSS priority (!important)."
   :group 'web-mode-faces)
 
 (defface web-mode-css-function-face
-  '((t (:inherit font-lock-builtin-face)))
+  '((t :inherit font-lock-builtin-face))
   "Face for CSS functions."
   :group 'web-mode-faces)
 
 (defface web-mode-variable-name-face
-  '((t (:inherit font-lock-variable-name-face)))
+  '((t :inherit font-lock-variable-name-face))
   "Face for variable names."
   :group 'web-mode-faces)
 
 (defface web-mode-function-name-face
-  '((t (:inherit font-lock-function-name-face)))
+  '((t :inherit font-lock-function-name-face))
   "Face for function names."
   :group 'web-mode-faces)
 
 (defface web-mode-function-call-face
-  '((t (:inherit font-lock-function-name-face)))
+  '((t :inherit font-lock-function-name-face))
   "Face for function calls."
   :group 'web-mode-faces)
 
 (defface web-mode-string-face
-  '((t (:inherit font-lock-string-face)))
+  '((t :inherit font-lock-string-face))
   "Face for strings."
   :group 'web-mode-faces)
 
 (defface web-mode-block-string-face
-  '((t (:inherit web-mode-string-face)))
+  '((t :inherit web-mode-string-face))
   "Face for block strings."
   :group 'web-mode-faces)
 
 (defface web-mode-part-string-face
-  '((t (:inherit web-mode-string-face)))
+  '((t :inherit web-mode-string-face))
   "Face for part strings."
   :group 'web-mode-faces)
 
 (defface web-mode-javascript-string-face
-  '((t (:inherit web-mode-string-face)))
+  '((t :inherit web-mode-string-face))
   "Face for javascript strings."
   :group 'web-mode-faces)
 
 (defface web-mode-css-string-face
-  '((t (:inherit web-mode-string-face)))
+  '((t :inherit web-mode-string-face))
   "Face for css strings."
   :group 'web-mode-faces)
 
 (defface web-mode-json-key-face
-  '((t (:foreground "plum")))
+  '((t :foreground "plum"))
   "Face for json key strings."
   :group 'web-mode-faces)
 
 (defface web-mode-json-context-face
-  '((t (:foreground "orchid3")))
+  '((t :foreground "orchid3"))
   "Face for json context strings."
   :group 'web-mode-faces)
 
 (defface web-mode-json-string-face
-  '((t (:inherit web-mode-string-face)))
+  '((t :inherit web-mode-string-face))
   "Face for json strings."
   :group 'web-mode-faces)
 
 (defface web-mode-comment-face
-  '((t (:inherit font-lock-comment-face)))
+  '((t :inherit font-lock-comment-face))
   "Face for comments."
   :group 'web-mode-faces)
 
 (defface web-mode-block-comment-face
-  '((t (:inherit web-mode-comment-face)))
+  '((t :inherit web-mode-comment-face))
   "Face for server comments."
   :group 'web-mode-faces)
 
 (defface web-mode-part-comment-face
-  '((t (:inherit web-mode-comment-face)))
+  '((t :inherit web-mode-comment-face))
   "Face for part comments."
   :group 'web-mode-faces)
 
@@ -480,37 +480,37 @@ with value 2, HTML lines beginning text are also indented (do not forget side ef
   :group 'web-mode-faces)
 
 (defface web-mode-javascript-comment-face
-  '((t (:inherit web-mode-comment-face)))
+  '((t :inherit web-mode-comment-face))
   "Face for javascript comments."
   :group 'web-mode-faces)
 
 (defface web-mode-css-comment-face
-  '((t (:inherit web-mode-comment-face)))
+  '((t :inherit web-mode-comment-face))
   "Face for css comments."
   :group 'web-mode-faces)
 
 (defface web-mode-constant-face
-  '((t (:inherit font-lock-constant-face)))
+  '((t :inherit font-lock-constant-face))
   "Face for language constants."
   :group 'web-mode-faces)
 
 (defface web-mode-type-face
-  '((t (:inherit font-lock-type-face)))
+  '((t :inherit font-lock-type-face))
   "Face for language types."
   :group 'web-mode-faces)
 
 (defface web-mode-keyword-face
-  '((t (:inherit font-lock-keyword-face)))
+  '((t :inherit font-lock-keyword-face))
   "Face for language keywords."
   :group 'web-mode-faces)
 
 (defface web-mode-param-name-face
-  '((t (:foreground "Snow3")))
+  '((t :foreground "Snow3"))
   "Face for server attribute names."
   :group 'web-mode-faces)
 
 (defface web-mode-whitespace-face
-  '((t (:background "DarkOrchid4")))
+  '((t :background "DarkOrchid4"))
   "Face for whitespaces."
   :group 'web-mode-faces)
 
@@ -533,22 +533,22 @@ Must be used in conjunction with web-mode-enable-block-face."
   :group 'web-mode-faces)
 
 (defface web-mode-part-face
-  '((t (:inherit web-mode-block-face)))
+  '((t :inherit web-mode-block-face))
   "Face for parts."
   :group 'web-mode-faces)
 
 (defface web-mode-folded-face
-  '((t (:underline t)))
+  '((t :underline t))
   "Overlay face for folded."
   :group 'web-mode-faces)
 
 (defface web-mode-current-element-highlight-face
-  '((t (:background "#000000")))
+  '((t :background "#000000"))
   "Overlay face for element highlight."
   :group 'web-mode-faces)
 
 (defface web-mode-comment-keyword-face
-  '((t (:weight bold :box t)))
+  '((t :weight bold :box t))
   "Comment keywords."
   :group 'web-mode-faces)
 
