8c8a9a6fe0e424a048e759fc09ea05625a0f6795
François-Xavier Bois <fxbois@gmail.com>
2014-08-05 08:53:55 +0200

    more generic batch.sh

--- unit-test/batch.sh
+++ unit-test/batch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-# on peut avoir plusieurs --eval '(message "coucou")'
-# -l init.el
+if [ -x /Applications/Emacs.app/Contents/MacOS/Emacs ]; 
+then
+    EMACS=/Applications/Emacs.app/Contents/MacOS/Emacs
+else
+    EMACS=emacs
+fi
 
-/Applications/Emacs.app/Contents/MacOS/Emacs -batch -no-site-file -Q --eval '(load-file "../web-mode.el")' --eval '(web-mode)' -f web-mode-test
+$EMACS -batch -no-site-file -Q --eval '(load-file "../web-mode.el")' --eval '(web-mode)' -f web-mode-test
 
