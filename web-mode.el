b48385f1a352f6d5b71eca5c1b065cb1034aa61c
Aaron Cimolini <aaron@cimolini.com>
2014-09-19 15:10:08 -0400

    Allows for proper indentation with capture and unless tags

--- web-mode.el
+++ web-mode.el
@@ -1193,7 +1193,7 @@ Must be used in conjunction with web-mode-enable-block-face."
      "elif" "else" "elseif" "elsif" "embed" "empty" "filter" "foreach" "for"
      "ifchanged" "ifequal" "ifnotequal" "if" "with"
      "macro" "draw" "random" "safe" "sandbox" "spaceless" "verbatim"
-     "form" ;; liquid
+     "form" "unless" "capture" ;; liquid
      )
    t))
 
