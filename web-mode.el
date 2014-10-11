9091f92ca8d10632900a5d1beeb1121660e2731d
François-Xavier Bois <fxbois@gmail.com>
2014-10-11 18:26:12 +0200

    simplify licensing
    a LICENSE file now exists at the root of the repo

--- web-mode.el
+++ web-mode.el
@@ -10,27 +10,12 @@
 ;; Keywords: html template javascript css web php django erb jsp
 ;; URL: http://web-mode.org
 ;; Repository: http://github.com/fxbois/web-mode
+;; License: GNU General Public License >= 2
+;; Distribution: This file is not part of Emacs
 
 ;; =========================================================================
 ;; This work is sponsored by Kernix : Digital Agency (Web & Mobile) in Paris
 ;; =========================================================================
-
-;; This file is not part of Emacs
-
-;; This file is free software; you can redistribute it and/or modify
-;; it under the terms of the GNU General Public License as published by
-;; the Free Software Foundation; either version 2, or (at your option)
-;; any later version.
-
-;; This file is distributed in the hope that it will be useful,
-;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;; GNU General Public License for more details.
-
-;; You should have received a copy of the GNU General Public License
-;; along with GNU Emacs; see the file COPYING.  If not, write to
-;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
-;; Boston, MA 02111-1307, USA.
 
 ;; Code goes here
 
