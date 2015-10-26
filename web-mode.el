8fa43f8dc9e92bb83600621fd81da705c03ddb45
fxbois <fxbois@gmail.com>
2015-10-26 01:12:55 +0100

    v12.4

--- web-mode.el
+++ web-mode.el
@@ -3,7 +3,7 @@
 
 ;; Copyright 2011-2015 François-Xavier Bois
 
-;; Version: 12.3.14
+;; Version: 12.4.0
 ;; Author: François-Xavier Bois <fxbois AT Google Mail Service>
 ;; Maintainer: François-Xavier Bois
 ;; Created: July 2011
@@ -26,7 +26,7 @@
 
 ;;---- CONSTS ------------------------------------------------------------------
 
-(defconst web-mode-version "12.3.14"
+(defconst web-mode-version "12.4.0"
   "Web Mode version.")
 
 ;;---- GROUPS ------------------------------------------------------------------
