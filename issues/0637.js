1a6a5ab86ff95d39facc537ca344f28465febac8
fxbois <fxbois@gmail.com>
2015-11-28 19:21:24 +0100

    syntax-table fix

---
+++ issues/0637.js
@@ -0,0 +1,14 @@
+function test() {
+  switch (q) {
+    case 'a':
+      return a;
+    case 'b':
+      return b;
+    case 'c':
+      return c;
+    case 'd':
+      return d;
+    default:
+      return [];
+  }
+}
