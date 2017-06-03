290f0ecb43dc6a636e45f55cf95c0d8fbda3fdc2
fxbois <fxbois@gmail.com>
2017-06-03 15:32:27 +0200

    ruby indent fix
    #883 #884

--- web-mode.el
+++ web-mode.el
@@ -3,7 +3,7 @@
 
 ;; Copyright 2011-2017 François-Xavier Bois
 
-;; Version: 15.0.0
+;; Version: 15.0.1
 ;; Author: François-Xavier Bois <fxbois AT Google Mail Service>
 ;; Maintainer: François-Xavier Bois
 ;; Package-Requires: ((emacs "23.1"))
@@ -24,7 +24,7 @@
 
 ;;---- CONSTS ------------------------------------------------------------------
 
-(defconst web-mode-version "15.0.0"
+(defconst web-mode-version "15.0.1"
   "Web Mode version.")
 
 ;;---- GROUPS ------------------------------------------------------------------
@@ -7958,10 +7958,10 @@ another auto-completion with different ac-sources (e.g. ac-php)")
         ;;(message "%S" prev-line)
         (setq prev-indentation (cdr h))
         (cond
-         ((string-match-p "^\\(end\\|else\\|elsif\\|when\\)" line)
+         ((string-match-p "^[ ]*\\(end\\|else\\|elsif\\|when\\)" line)
           (setq offset (- prev-indentation language-offset))
           )
-         ((string-match-p "\\(when\\|if\\|else\\|elsif\\|unless\\|for\\|while\\|def\\|class\\)" prev-line)
+         ((string-match-p "^[ ]*\\(when\\|if\\|else\\|elsif\\|unless\\|for\\|while\\|def\\|class\\)" prev-line)
           (setq offset (+ prev-indentation language-offset))
           )
          (t
