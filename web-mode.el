cb55bd980d565488e3cf19dee4a7a4148d790676
Jimmy Yuen Ho Wong <wyuenho@gmail.com>
2017-11-12 15:49:34 +0000

    Fix customization types

--- web-mode.el
+++ web-mode.el
@@ -60,14 +60,16 @@
 
 (defcustom web-mode-attr-indent-offset nil
   "Html attribute indentation level."
-  :type 'integer
-  :safe #'integerp
+  :type '(choice (integer :tags "Number of spaces")
+                 (const :tags "Default" nil))
+  :safe #'(lambda (v) (or (integerp v) (booleanp v)))
   :group 'web-mode)
 
 (defcustom web-mode-attr-value-indent-offset nil
   "Html attribute value indentation level."
-  :type 'integer
-  :safe #'integerp
+  :type '(choice (integer :tags "Number of spaces")
+                 (const :tags "Default" nil))
+  :safe #'(lambda (v) (or (integerp v) (booleanp v)))
   :group 'web-mode)
 
 (defcustom web-mode-markup-indent-offset
@@ -181,7 +183,7 @@ See web-mode-block-face."
 
 (defcustom web-mode-enable-comment-interpolation t
   "Enable highlight of keywords like FIXME, TODO, etc. in comments."
-  :type 'list
+  :type 'boolean
   :group 'web-mode)
 
 (defcustom web-mode-enable-string-interpolation t
@@ -222,14 +224,14 @@ See web-mode-block-face."
 (defcustom web-mode-comment-style 1
   "Comment style : 1 = default, 2 = force server comments outside a block."
   :group 'web-mode
-  :type '(choice (const :tag "default" 1)
-                 (const :tag "force engine comments" 2)))
+  :type '(choice (const :tag "Default" 1)
+                 (const :tag "Force engine comments" 2)))
 
 (defcustom web-mode-indent-style 2
   "Indentation style."
   :group 'web-mode
-  :type '(choice (const :tag "default (all lines are indented)" 2)
-                 (const :tag "text at the beginning of line is not indented" 1)))
+  :type '(choice (const :tag "Default (all lines are indented)" 2)
+                 (const :tag "Text at the beginning of line is not indented" 1)))
 
 (defcustom web-mode-auto-close-style 1
   "Auto-close style."
@@ -245,47 +247,47 @@ See web-mode-block-face."
 
 (defcustom web-mode-extra-expanders '()
   "A list of additional expanders."
-  :type 'list
+  :type '(alist :key-type string :value-type string)
   :group 'web-mode)
 
 (defcustom web-mode-extra-auto-pairs '()
   "A list of additional snippets."
-  :type 'list
+  :type '(alist :key-type string :value-type string)
   :group 'web-mode)
 
 (defcustom web-mode-extra-snippets '()
   "A list of additional snippets."
-  :type 'list
+  :type '(alist :key-type string :value-type string)
   :group 'web-mode)
 
 (defcustom web-mode-extra-builtins '()
   "A list of additional builtins."
-  :type 'list
+  :type '(alist :key-type string :value-type string)
   :group 'web-mode)
 
 (defcustom web-mode-extra-constants '()
   "A list of additional constants."
-  :type 'list
+  :type '(alist :key-type string :value-type string)
   :group 'web-mode)
 
 (defcustom web-mode-extra-keywords '()
   "A list of additional keywords."
-  :type 'list
+  :type '(alist :key-type string :value-type string)
   :group 'web-mode)
 
 (defcustom web-mode-extra-types '()
   "A list of additional types."
-  :type 'list
+  :type '(alist :key-type string :value-type string)
   :group 'web-mode)
 
 (defcustom web-mode-extra-control-blocks '()
   "A list of additional control blocks."
-  :type 'list
+  :type '(alist :key-type string :value-type string)
   :group 'web-mode)
 
 (defcustom web-mode-tests-directory (concat default-directory "tests/")
   "Directory containing all the unit tests."
-  :type 'list
+  :type 'directory
   :group 'web-mode)
 
 (defcustom web-mode-jsx-depth-faces nil
@@ -294,7 +296,7 @@ See web-mode-block-face."
   ;;  web-mode-jsx-depth-3-face
   ;;  web-mode-jsx-depth-4-face)
   "Each jsx depth has is own face."
-  :type 'list
+  :type '(repeat face)
   :group 'web-mode)
 
 (defcustom web-mode-commands-like-expand-region
