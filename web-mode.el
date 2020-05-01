d115f8dc3052e5779938d782d9cdaa4533ef20ff
fxbois <fxbois@gmail.com>
2020-05-01 11:30:01 +0200

    web-mode v17

--- web-mode.el
+++ web-mode.el
@@ -3,7 +3,7 @@
 
 ;; Copyright 2011-2020 François-Xavier Bois
 
-;; Version: 16.0.41
+;; Version: 17.0.0
 ;; Author: François-Xavier Bois <fxbois AT Google Mail Service>
 ;; Maintainer: François-Xavier Bois
 ;; Package-Requires: ((emacs "23.1"))
@@ -24,7 +24,7 @@
 
 ;;---- CONSTS ------------------------------------------------------------------
 
-(defconst web-mode-version "16.0.41"
+(defconst web-mode-version "17.0.0"
   "Web Mode version.")
 
 ;;---- GROUPS ------------------------------------------------------------------
