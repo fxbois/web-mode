a6f495474ff02e1a3d367cd2df6b9ab11e2545dd
fxbois <fxbois@gmail.com>
2024-12-26 10:05:19 +0100

    Update 1310.jsx

--- issues/1310.jsx
+++ issues/1310.jsx
@@ -8,4 +8,19 @@ function App() {
       </div>
     </>
   );
-}
+};
+
+const Translations = () => {
+  const { t } = useTranslation("coi-platform-translations", {
+    keyPrefix: keyPrefixes.main,
+  });
+  return (
+    <>
+      <Alert severity="info" icon={false}>
+        <AlertTitle>{t("info_title")}</AlertTitle>
+        <Typography variant="body2">{t("info_description")}</Typography>
+      </Alert>
+      <EditForm />
+    </>
+  );
+};
